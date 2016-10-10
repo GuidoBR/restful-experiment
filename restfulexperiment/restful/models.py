c6e1a9070f7be986e897e9083f6219ffad533902
Guido <guidopercu@gmail.com>
2016-10-09 21:29:59 -0300

    Creating token for users

--- restfulexperiment/restful/models.py
+++ restfulexperiment/restful/models.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*- 
 from django.db import models
+from rest_framework_jwt.settings import api_settings
 
 class User(models.Model):
     name = models.CharField(max_length=254)
@@ -10,6 +11,17 @@ class User(models.Model):
     modified = models.DateField(auto_now=True)
     last_login = models.DateField(auto_now_add=True)
     token = models.CharField(max_length=254)
+    username = models.EmailField(max_length=254, default='default_user')
 
     def __str__(self):
         return "{} - {}".format(self.name, self.email)
+
+    def save(self, *args, **kwargs):
+        self.username = self.email
+
+        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+        payload = jwt_payload_handler(self)
+        self.token = jwt_encode_handler(payload)
+        super().save(*args, **kwargs)
